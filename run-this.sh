9dd981081efc7984dd6d9945111bc29fb083963e
riffvuln <budeksek@gmail.com>
2025-04-05 14:10:58 +0700

    Update run-this.sh

--- run-this.sh
+++ run-this.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-sudo apt install rustup gcc
-sudo rustup update nightly
-sudo rustup toolchain install nightly
+install_rust() {
+  sudo apt install rustup gcc
+  sudo rustup update nightly
+  sudo rustup default nightly
+  sudo rustup toolchain install nightly
+}
+
+install_rust
