5a491dd7bfbdfcdac4b7f4f9a62606e7aaabe0a7
riffvuln <budeksek@gmail.com>
2025-04-05 14:08:23 +0700

    Update run-this.sh

--- run-this.sh
+++ run-this.sh
@@ -1,27 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
-# Function to install a package and check for errors
-install_package() {
-  sudo apt install -y "$1"
-  if [ $? -ne 0 ]; then
-    echo "Failed to install $1"
-    exit 1
-  fi
-}
-
-# Function to run a command and check for errors
-run_command() {
-  eval "$1"
-  if [ $? -ne 0 ]; then
-    echo "Failed to execute: $1"
-    exit 1
-  fi
-}
-
-# Install required packages
-install_package "rustup"
-install_package "gcc"
-
-# Update rustup and add target
-run_command "sudo rustup update nightly"
-run_command "sudo rustup target add x86_64-unknown-linux-gnu"
+sudo apt install rustup gcc
+sudo rustup update nightly
+sudo rustup toolchain install nightly
